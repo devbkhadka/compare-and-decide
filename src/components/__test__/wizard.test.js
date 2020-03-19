8c6789a3d08e109ca896aa15fd7a0d9b956a648a
Dev Khadka <devbahadurkhadka@SMS-EXE-Dev-2.local>
2020-03-19 16:55:21 +0545

    added functionality of next and previous button on the wizard

--- src/components/__test__/wizard.test.js
+++ src/components/__test__/wizard.test.js
@@ -4,6 +4,10 @@ import { render, fireEvent } from '@testing-library/react'
 import TabbedWizard from '../wizard'
 
 describe("Test Wizard Component", ()=>{
+    const wizardWithContent = <TabbedWizard tabs={['tab1', 'tab2']}>
+                                <div>Tab1 content</div>
+                                <div>Tab2 content</div>
+                            </TabbedWizard>
     it("should render tabs correctly", ()=> {
         const wizard = render(<TabbedWizard tabs={['tab1', 'tab2']}></TabbedWizard>)
         
@@ -12,39 +16,53 @@ describe("Test Wizard Component", ()=>{
     })
 
     it("should show first panel when rendered", ()=> {
-        const wizard = render(
-            <TabbedWizard tabs={['tab1', 'tab2']}>
-                <div>Tab1 content</div>
-                <div>Tab2 content</div>
-            </TabbedWizard>
-        )
+        const wizard = render(wizardWithContent)
 
         wizard.getByText('Tab1 content')
 
         expect(wizard.queryByText('Tab2 content')).toBe(null)
     })
 
-    it("clicking on tab should change the tab", ()=> {
-        const wizard = render(
-            <TabbedWizard tabs={['tab1', 'tab2']}>
-                <div>Tab1 content</div>
-                <div>Tab2 content</div>
-            </TabbedWizard>
-        )
+    test("clicking on tab should change the tab", ()=> {
+        const wizard = render(wizardWithContent)
 
         fireEvent.click(wizard.getByTestId('tab-1'))
 
         wizard.getByText('Tab2 content')
+
+        expect(wizard.queryByText('Tab1 content')).toBe(null)
         
     })
 
+    test("clicking next buttion takes to next tab", () => {
+        const wizard = render(wizardWithContent)
+
+        const nextButton = wizard.getByTestId('next')
+
+        fireEvent.click(nextButton)
+        wizard.getByText('Tab2 content')
+
+        // clicking again at last step will not change tab
+        fireEvent.click(nextButton)
+        wizard.getByText('Tab2 content')
+    })
+
+    test("clicking prev buttion takes to previous tab", () => {
+        const wizard = render(wizardWithContent)
+
+        fireEvent.click(wizard.getByTestId('tab-1'))
+        const prevButton = wizard.getByTestId('prev')
+
+        fireEvent.click(prevButton)
+        wizard.getByText('Tab1 content')
+
+        // clicking again at 1st step will not change tab
+        fireEvent.click(prevButton)
+        wizard.getByText('Tab1 content')
+    })
+
     test('Wizard snapshot is not changed', ()=>{
-        const wizard = render(
-            <TabbedWizard tabs={['tab1', 'tab2']}>
-                <div>Tab1 content</div>
-                <div>Tab2 content</div>
-            </TabbedWizard>
-        )
+        const wizard = render(wizardWithContent)
 
         expect(wizard.baseElement).toMatchSnapshot()
     })
--- src/components/wizard.js
+++ src/components/wizard.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-import { Paper, Tabs, Tab } from '@material-ui/core'
+import { Paper, Tabs, Tab, Button } from '@material-ui/core'
 import TabPanel from './tab_panel'
 
 export default function TabbedWizard({tabs, children}) {
     const [value, setValue] = React.useState(0)
     const onTabChange = (event, newValue)=>{
         setValue(newValue)
+    }
+    const stepTabBy = (count) => {
+        const newValue = value + count
+        if(newValue>=0 && newValue<children.length) {
+            setValue(newValue)
+        }
     }
     return <div>
         <Paper>
@@ -25,6 +31,8 @@ export default function TabbedWizard({tabs, children}) {
                 </TabPanel>
             }) }
         </div>
+        <Button data-testid="prev" onClick={()=>stepTabBy(-1)}>Previous</Button>
+        <Button data-testid="next" onClick={()=>stepTabBy(1)}>Next</Button>
     </div>
 }
 
