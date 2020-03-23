2855a675b2b0a777e18fd6dac79b9d2f350d7bd3
Dev Khadka <devbahadurkhadka@SMS-EXE-Dev-2.local>
2020-03-23 21:45:50 +0545

    created basic layout for add item component

--- src/components/wizard.js
+++ src/components/wizard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Paper, Tabs, Tab, Box, makeStyles } from '@material-ui/core'
+import { Paper, Tabs, Tab, Box, makeStyles, IconButton } from '@material-ui/core'
 import {ArrowBack, ArrowForward} from '@material-ui/icons';
 import TabPanel from './tab_panel'
 
@@ -46,16 +46,20 @@ export default function TabbedWizard({tabs, children}) {
                 {tabs.map((tab, i)=><Tab data-testid={'tab-' + i} key={i} label={tab}></Tab>)}
             </Tabs>
         </Paper>
-        <Box flexGrow={1} className={classes.tabPanel}>
+        <Box flexGrow={1}>
             { children && children.map((child,i)=>{
-                return <TabPanel index={i} key={i} value={value}>
+                return <TabPanel index={i} key={i} value={value} className={classes.tabPanel}>
                     {child}
                 </TabPanel>
             }) }
         </Box>
         <Box display='flex' justifyContent='space-between' className={classes.buttonContainer}>
-            <ArrowBack  fontSize='large' data-testid="prev" onClick={()=>stepTabBy(-1)}/>
-            <ArrowForward fontSize='large'  data-testid="next" onClick={()=>stepTabBy(1)}/>
+            <IconButton data-testid="prev" onClick={()=>stepTabBy(-1)}>
+                <ArrowBack  fontSize='large' />
+            </IconButton>
+            <IconButton data-testid="next" onClick={()=>stepTabBy(1)}>
+                <ArrowForward fontSize='large'/>
+            </IconButton>
         </Box>
         
     </Box>
