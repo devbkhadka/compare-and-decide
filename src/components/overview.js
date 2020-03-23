f536f947c1f1c074a2e1e2c445fbd425fd3c030e
Dev Khadka <devbahadurkhadka@SMS-EXE-Dev-2.local>
2020-03-23 18:26:46 +0545

    applied them to app bar buttons and icons

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,17 +1,37 @@
 import React from 'react'
-import { Typography, Box} from '@material-ui/core'
+import { Typography, Box, useTheme} from '@material-ui/core'
+import {List, ListItem, ListItemText, ListItemIcon} from '@material-ui/core'
+import {Star} from '@material-ui/icons'
 
 export default function Overview(props) {
+    const steps = [
+        'List all items from which you want to decide from with its attribute',
+        'Select one prefered item based on intution',
+        'Now compare the current prefered item with each item one at a time.' +
+        'In each comparasion choose one item you prefer and eliminate other until all items are compared',
+        'Last prefered item remaining will be your choice. Hurray you have made the decision' 
+    ]
     return <Box>
-        <Typography variant='body1'>
-            This tool helps you choose one item from multiple items in three easy steps.
+        <Typography variant='h4' style={{marginBottom:20}}>
+            Make Decision in Three Easy Steps
         </Typography>
-        <ul>
-            <li>List all items and its attributes based on which you will do comparision</li>
-            <li>At first select any item you prefer based on your intusion</li>
-            <li>Now compare the current prefered item with each item one at a time. 
-                In each comparasion choose one item you prefere and eliminate other until all items are compared</li>
-            <li>Prefered item in the last comparision will be your choice</li>
-        </ul>
+        
+        <List>
+            {steps.map((text, i)=>(<BulletPoint text={text} key={i}></BulletPoint>))}
+        </List>
+        
     </Box>
+}
+
+
+function BulletPoint({text}) {
+    const theme = useTheme()
+    return <ListItem>
+            <ListItemIcon>
+                <Star />
+            </ListItemIcon>
+            <ListItemText
+                primary={text}
+            />
+        </ListItem>
 }
